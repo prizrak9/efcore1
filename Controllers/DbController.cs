52406cd4c539417e1200b91f86413e72783bdeb9
prizrak9 <pavelgolotuk@gmail.com>
2019-06-04 19:33:50 +0300

    added update, delete

--- Controllers/DbController.cs
+++ Controllers/DbController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using efcore1.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace efcore1.Controllers
 {
@@ -20,24 +21,6 @@ namespace efcore1.Controllers
         public IActionResult Index()
         {
             return PrintTable();
-
-            // var bills = context.Bills;
-
-            // var fields = typeof(Bills).GetProperties();
-
-            // var model = new TableProvider{
-            //     EntityType = typeof(Bills),
-            //     FieldNames = fields.Select(n => n.Name),
-            //     Values = bills.Select(n => fields.ToDictionary(m => m.Name, m => m.GetValue(n))),
-            //     AvailableTables = context.GetType()
-            //         .GetProperties()
-            //         .Where(n => n.PropertyType.IsGenericType)
-            //         .Select(n => n.Name)
-            //         .Distinct(),
-            // };
-
-
-            // return View(model);
         }
 
         public IActionResult Privacy()
@@ -58,6 +41,81 @@ namespace efcore1.Controllers
             return PrintTable(type);
         }
 
+        [HttpPost]
+        
+        // [ValidateAntiForgeryToken]
+        public ActionResult Update(int __index, string __entityType, IFormCollection collection)
+        {
+            try
+            {
+                Console.WriteLine("A");
+                var type = Type.GetType(__entityType);
+
+                var entities = SelectAll(type);
+
+                var entity = entities.ElementAt(__index);
+
+                var excludedNames = new[] { "__RequestVerificationToken", "__entityType", "__index" };
+
+                var values = collection
+                    .Where(n => !string.IsNullOrWhiteSpace(n.Value.FirstOrDefault()))
+                    .Where(n => !excludedNames.Contains(n.Key))
+                    .ToDictionary(n => n.Key, n => (object)n.Value.FirstOrDefault());
+
+                var fields = type.GetProperties()
+                    .Where(n => (n.PropertyType.IsValueType || n.PropertyType == typeof(string)));
+
+                foreach (var field in fields)
+                {
+                    var validTypeValue = Convert.ChangeType(values[field.Name], field.PropertyType);
+
+                    field.SetValue(entity, validTypeValue);
+                }
+
+                context.Update(entity);
+
+                context.SaveChanges();
+
+                // return RedirectToAction("Index");
+                return ShowTable(__entityType);
+            }
+            catch(Exception e)
+            {
+                ViewData["Message"] = e.Message;
+
+                return ShowTable(__entityType);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int __index, string __entityType)
+        {
+            try
+            {
+                var type = Type.GetType(__entityType);
+
+                var entities = SelectAll(type);
+
+                var entity = entities.ElementAt(__index);
+
+                context.Remove(entity);
+
+                context.SaveChanges();
+
+                return ShowTable(__entityType);
+            }
+            catch(Exception e)
+            {
+                ViewData["Message"] = e.Message;
+
+                return ShowTable(__entityType);
+            }
+        }
+        private IEnumerable<object> SelectAll(Type type)
+        {
+            return (IEnumerable<object>) context.GetType().GetProperty(type.Name).GetValue(context);
+        }
+
         public ActionResult PrintTable(Type type = null)
         {
             if (type is null)
@@ -72,11 +130,11 @@ namespace efcore1.Controllers
             }
 
             var entities = (IEnumerable<object>)context
-                    .GetType()
-                    .GetProperties()
-                    .Where(n => n.PropertyType.IsGenericType)
-                    .First(n => n.PropertyType.GetGenericArguments()[0] == type)
-                    .GetGetMethod().Invoke(context, null);
+                .GetType()
+                .GetProperties()
+                .Where(n => n.PropertyType.IsGenericType)
+                .First(n => n.PropertyType.GetGenericArguments()[0] == type)
+                .GetGetMethod().Invoke(context, null);
 
             var fields = type.GetProperties()
                 .Where(n => (n.PropertyType.IsValueType || n.PropertyType == typeof(string)));
